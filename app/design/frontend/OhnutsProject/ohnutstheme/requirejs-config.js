8965af4da149f8782be6dc41b130a297d681a14d
Shrikant Nannaware <shrikant@Office.Arista>
2022-05-03 20:13:14 +0530

    magento243 upload file folder

---
+++ app/design/frontend/OhnutsProject/ohnutstheme/requirejs-config.js
@@ -0,0 +1,14 @@
+var config = {
+    paths: {
+        slick: 'js/slick',
+        custom_ohnutsjs:"js/custom_ohnuts"
+    },
+    'shim': {
+       'slick': {
+            deps: ['jquery']
+        },
+        'custom_ohnuts': {
+            deps: ['jquery']
+        }
+    }
+};
---
+++ app/design/frontend/OhnutsProject/ohnutstheme/web/js/custom_ohnuts.js
@@ -0,0 +1,10 @@
+  require([ "jquery", 'jquery/ui'], function($){
+    "use strict";
+    var jQuery = $.noConflict();
+
+
+jQuery(document).ready( function(){
+
+
+});
+});
